8d28ef447afa5e96dbc4959172a9b1b2a9a16c32
Koki Ibukuro <koki.ibukuro@gmail.com>
2020-12-29 22:38:17 +0100

    Initial commit

---
+++ Assets/TutorialUIHole/Resources/UIHole.shader
@@ -0,0 +1,76 @@
+﻿Shader "UIHole"
+{
+    Properties
+    {
+        [PerRendererData] _MainTex("Sprite Texture", 2D) = "white" {}
+        _Color("Tint", Color) = (1,1,1,1)
+    }
+    
+    SubShader
+    {
+        Tags
+        { 
+            "Queue" = "Transparent"
+            "IgnoreProjector" = "True"
+            "RenderType" = "Transparent"
+            "PreviewType" = "Plane"
+        }
+        Cull Off
+        Lighting Off
+        ZWrite Off
+        Blend SrcAlpha OneMinusSrcAlpha
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float4 color : COLOR;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                fixed4 color : COLOR;
+                float4 vertex : SV_POSITION;
+            };
+
+            fixed4 _Color;
+
+            // xy: hole center
+            // z: hole size
+            // w: INVERTED aspect
+            fixed4 _HoleData;
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                o.color = v.color * _Color;
+                return o;
+            }
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                fixed4 col = i.color;
+
+                fixed2 vec = i.uv- _HoleData.xy;
+                vec.y *= _HoleData.w; // aspect collection
+                if (length(vec) < _HoleData.z)
+                {
+                    discard;
+                }
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/TutorialUIHole/Scripts/UIHole.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+[ExecuteInEditMode()]
+public class UIHole : Graphic, ICanvasRaycastFilter
+{
+    [SerializeField] private Vector2 _center;
+    [SerializeField, Range(0f, 2f)] private float _size;
+
+    private static readonly int _HoleCenter = Shader.PropertyToID("_HoleData");
+    private Material _material;
+    private bool _needUpdate = false;
+
+    public Vector2 Center
+    {
+        get => _center;
+        set
+        {
+            _center = value;
+            _needUpdate = true;
+        }
+    }
+
+    public float Size
+    {
+        get => _size;
+        set
+        {
+            _size = value;
+            _needUpdate = true;
+        }
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        _material = m_Material;
+    }
+
+    protected override void OnDisable()
+    {
+        _material = null;
+        base.OnDisable();
+    }
+
+#if UNITY_EDITOR
+    protected override void OnValidate()
+    {
+        base.OnValidate();
+        _needUpdate = true;
+    }
+#endif // UNITY_EDITOR
+
+    private void Update()
+    {
+        if (_material == null)
+        {
+            _material = m_Material;
+        }
+        if (!_needUpdate) return;
+
+        // xy: hole center, z: hole size, w: aspect
+        Vector4 hole = new Vector4(
+            _center.x,
+            _center.y,
+            Mathf.Max(_size * 0.5f, 0),
+            (float)Screen.height / Screen.width // INVERTED aspect
+        );
+        _material.SetVector(_HoleCenter, hole);
+
+        _needUpdate = false;
+    }
+
+    // Block raycast
+    public bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
+    {
+        return !IsHole(sp);
+    }
+
+    private bool IsHole(Vector2 sp)
+    {
+        float width = Screen.width;
+        float height = Screen.height;
+
+        Vector2 viewport = new Vector2(sp.x / width, sp.y / height);
+        Vector2 vec = viewport - _center;
+        vec.y *= height / width;// aspect collection
+
+        return vec.magnitude < (_size * 0.5f);
+    }
+}
